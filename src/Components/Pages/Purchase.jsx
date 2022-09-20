75f9df6ac75c0e7bd1dce29890264fbd683423d1
Jens Harder <76961272+Jens180980@users.noreply.github.com>
2022-09-20 11:00:40 +0200

    init seatpicker

--- src/Components/Pages/Purchase.jsx
+++ src/Components/Pages/Purchase.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { SeatPicker } from "../Partials/SeatPicker";
 
 export const Purchase = () => {
-  return <div>Purchase</div>;
+  return (
+    <div>
+      <SeatPicker event_id="5" />
+    </div>
+  );
 };
--- src/Components/Partials/SeatPicker.jsx
+++ src/Components/Partials/SeatPicker.jsx
@@ -1,5 +1,133 @@
-import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Style from "../../Assets/scss/SeatPicker.module.scss";
 
-export const SeatPicker = () => {
-  return <div>SeatPicker</div>;
+export const SeatPicker = ({ event_id }) => {
+  const [SeatData, setSeatData] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      const result = await axios.get(
+        `https://api.mediehuset.net/detutroligeteater/seats/${event_id}`
+      );
+
+      setSeatData(result.data.items);
+    };
+    getData();
+  }, [event_id]);
+  console.log(SeatData);
+
+  return (
+    <section className={Style.SeatPickerWrapper}>
+      <p>{event_id}</p>
+      <div className={Style.line}>
+        {SeatData &&
+          SeatData.map((item) => {
+            switch (item.line) {
+              case "1":
+                return (
+                  <div
+                    key={item.id}
+                    className={
+                      item.is_reserved > 0
+                        ? Style.thisSeatIsTaken
+                        : Style.youCanSitHereIfYouWantTo
+                    }
+                  ></div>
+                );
+            }
+          })}
+      </div>
+      <div className={Style.line}>
+        {SeatData &&
+          SeatData.map((item) => {
+            switch (item.line) {
+              case "2":
+                return (
+                  <div
+                    key={item.id}
+                    className={
+                      item.is_reserved > 0
+                        ? Style.thisSeatIsTaken
+                        : Style.youCanSitHereIfYouWantTo
+                    }
+                  ></div>
+                );
+            }
+          })}
+      </div>
+      <div className={Style.line}>
+        {SeatData &&
+          SeatData.map((item) => {
+            switch (item.line) {
+              case "3":
+                return (
+                  <div
+                    key={item.id}
+                    className={
+                      item.is_reserved > 0
+                        ? Style.thisSeatIsTaken
+                        : Style.youCanSitHereIfYouWantTo
+                    }
+                  ></div>
+                );
+            }
+          })}
+      </div>
+      <div className={Style.line}>
+        {SeatData &&
+          SeatData.map((item) => {
+            switch (item.line) {
+              case "4":
+                return (
+                  <div
+                    key={item.id}
+                    className={
+                      item.is_reserved > 0
+                        ? Style.thisSeatIsTaken
+                        : Style.youCanSitHereIfYouWantTo
+                    }
+                  ></div>
+                );
+            }
+          })}
+      </div>
+      <div className={Style.line}>
+        {SeatData &&
+          SeatData.map((item) => {
+            switch (item.line) {
+              case "5":
+                return (
+                  <div
+                    key={item.id}
+                    className={
+                      item.is_reserved > 0
+                        ? Style.thisSeatIsTaken
+                        : Style.youCanSitHereIfYouWantTo
+                    }
+                  ></div>
+                );
+            }
+          })}
+      </div>
+      <div className={Style.line}>
+        {SeatData &&
+          SeatData.map((item) => {
+            switch (item.line) {
+              case "6":
+                return (
+                  <div
+                    key={item.id}
+                    className={
+                      item.is_reserved > 0
+                        ? Style.thisSeatIsTaken
+                        : Style.youCanSitHereIfYouWantTo
+                    }
+                  ></div>
+                );
+            }
+          })}
+      </div>
+    </section>
+  );
 };
